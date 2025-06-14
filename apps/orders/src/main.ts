703adb4446b6cf952192a22abc9f716b7fa78986
Ambrose Ahawo <ambroseahawo@gmail.com>
2025-06-14 15:27:32 +0300

    Implement config service

--- apps/orders/src/main.ts
+++ apps/orders/src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from "@nestjs/core";
 
 import { OrdersModule } from "@app/orders/orders.module";
+import { ValidationPipe } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(OrdersModule);
-  await app.listen(process.env.port ?? 3000);
+  app.useGlobalPipes(new ValidationPipe());
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>("PORT");
+
+  await app.listen(port as number);
 }
 bootstrap();
