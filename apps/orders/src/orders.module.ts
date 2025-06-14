59aaf16fa2e5975660515fb6b7ba29b0de3d1264
Ambrose Ahawo <ambroseahawo@gmail.com>
2025-06-14 07:35:32 +0300

    Implement orders repository

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -1,10 +1,26 @@
-import { Module } from '@nestjs/common';
-import { OrdersController } from './orders.controller';
-import { OrdersService } from './orders.service';
+import { DatabaseModule } from "@app/common";
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { MongooseModule } from "@nestjs/mongoose";
+import * as Joi from "joi";
+import { OrdersController } from "./orders.controller";
+import { OrdersRepository } from "./orders.repository";
+import { OrdersService } from "./orders.service";
+import { Order, orderSchema } from "./schemas/order.schema";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+      }),
+      envFilePath: "./apps/orders/.env",
+    }),
+    DatabaseModule,
+    MongooseModule.forFeature([{ name: Order.name, schema: orderSchema }]),
+  ],
   controllers: [OrdersController],
-  providers: [OrdersService],
+  providers: [OrdersService, OrdersRepository],
 })
 export class OrdersModule {}
