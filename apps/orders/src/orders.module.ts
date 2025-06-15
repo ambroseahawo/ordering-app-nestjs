30bf66e54e6b9df071d2aeadea95650edf03c1ae
Ambrose Ahawo <ambroseahawo@gmail.com>
2025-06-15 21:53:13 +0300

    Update orders app

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -1,10 +1,11 @@
 import * as Joi from "joi";
 
-import { DatabaseModule } from "@app/common";
+import { DatabaseModule, RmqModule } from "@app/common";
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { MongooseModule } from "@nestjs/mongoose";
 
+import { BILLING_SERVICE } from "@app/orders/constants/services";
 import { OrdersController } from "@app/orders/orders.controller";
 import { OrdersRepository } from "@app/orders/orders.repository";
 import { OrdersService } from "@app/orders/orders.service";
@@ -22,6 +23,7 @@ import { Order, orderSchema } from "@app/orders/schemas/order.schema";
     }),
     DatabaseModule,
     MongooseModule.forFeature([{ name: Order.name, schema: orderSchema }]),
+    RmqModule.register({ name: BILLING_SERVICE }),
   ],
   controllers: [OrdersController],
   providers: [OrdersService, OrdersRepository],
--- apps/orders/src/orders.service.ts
+++ apps/orders/src/orders.service.ts
@@ -1,13 +1,33 @@
-import { Injectable } from "@nestjs/common";
-import { CreateOrderRequest } from "./dto/order.dto";
-import { OrdersRepository } from "./orders.repository";
+import { lastValueFrom } from "rxjs";
+
+import { Inject, Injectable } from "@nestjs/common";
+import { ClientProxy } from "@nestjs/microservices";
+
+import { BILLING_SERVICE } from "@app/orders/constants/services";
+import { CreateOrderRequest } from "@app/orders/dto/order.dto";
+import { OrdersRepository } from "@app/orders/orders.repository";
 
 @Injectable()
 export class OrdersService {
-  constructor(private readonly ordersRepository: OrdersRepository) {}
+  constructor(
+    private readonly ordersRepository: OrdersRepository,
+    @Inject(BILLING_SERVICE) private billingClient: ClientProxy,
+  ) {}
 
   async createOrder(request: CreateOrderRequest) {
-    return this.ordersRepository.create(request);
+    // return this.ordersRepository.create(request);
+    const session = await this.ordersRepository.startTransaction();
+    try {
+      const order = await this.ordersRepository.create(request, { session });
+      await lastValueFrom(this.billingClient.emit("order_created", { request }));
+      await session.commitTransaction();
+      return order;
+    } catch (error) {
+      await session.abortTransaction();
+      throw error;
+    } finally {
+      session.endSession();
+    }
   }
 
   async getOrders() {
