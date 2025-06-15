286be0e3bea07b09125e5008dc5985a3c0e8d630
Ambrose Ahawo <ambroseahawo@gmail.com>
2025-06-15 21:53:00 +0300

    Update billing app

--- apps/billing/src/billing.controller.ts
+++ apps/billing/src/billing.controller.ts
@@ -1,12 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
-import { BillingService } from './billing.service';
+import { RmqService } from "@app/common";
+import { Controller } from "@nestjs/common";
+import { Ctx, EventPattern, Payload, RmqContext } from "@nestjs/microservices";
+
+import { BillingService } from "@app/billing/billing.service";
 
 @Controller()
 export class BillingController {
-  constructor(private readonly billingService: BillingService) {}
+  constructor(
+    private readonly billingService: BillingService,
+    private readonly rmqService: RmqService,
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.billingService.getHello();
+  @EventPattern("order_created")
+  async handleOrderCreated(@Payload() data: any, @Ctx() context: RmqContext) {
+    this.billingService.bill(data);
+    this.rmqService.ack(context);
+
+    // const channel = context.getChannelRef();
+    // const originalMsg = context.getMessage();
+
+    // try {
+    //   await this.billingService.bill(data);
+    //   channel.ack(originalMsg);
+    // } catch (error) {
+    //   channel.nack(originalMsg);
+    //   throw error;
+    // }
   }
 }
